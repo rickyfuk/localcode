62e4362d3a35b53d6dfb1d78fa7f0b08b9c0f137
digilau <rickyfukfuk@gmail.com>
2020-09-21 21:26:10 -0700

    update

--- 14-Full-Stack/04-Supplemental/Sequelize-Passport-Example/public/js/members.js
+++ 14-Full-Stack/04-Supplemental/Sequelize-Passport-Example/public/js/members.js
@@ -1,7 +1,16 @@
 $(document).ready(() => {
-  // This file just does a GET request to figure out which user is logged in
-  // and updates the HTML on the page
-  $.get("/api/user_data").then(data => {
-    $(".member-name").text(data.email);
-  });
+	// This file just does a GET request to figure out which user is logged in
+	// and updates the HTML on the page
+	// original code
+	$.get('/api/user_data').then((data) => {
+		$('.member-name').text(data.email);
+	});
+	// origianl code end
+
+	// try1 (this trial is try to use to send the data to html directly instead of passing through a get request)
+	// $.get('/').then((data) => {
+	// 	$('.member-name').text(data.email);
+	// });
+	// try 1 end
+	// findings: we can skip this code at all if we are using handlebars
 });
--- 14-Full-Stack/04-Supplemental/Sequelize-Passport-Example/routes/html-routes.js
+++ 14-Full-Stack/04-Supplemental/Sequelize-Passport-Example/routes/html-routes.js
@@ -1,29 +1,54 @@
 // Requiring path to so we can use relative routes to our HTML files
-const path = require("path");
+const path = require('path');
 
 // Requiring our custom middleware for checking if a user is logged in
-const isAuthenticated = require("../config/middleware/isAuthenticated");
+const isAuthenticated = require('../config/middleware/isAuthenticated');
 
 module.exports = function(app) {
-  app.get("/", (req, res) => {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
-  });
+	// original app.get '/'
+	app.get('/', (req, res) => {
+		// If the user already has an account send them to the members page
+		if (req.user) {
+			res.redirect('/members');
+		}
+		res.sendFile(path.join(__dirname, '../public/signup.html'));
+	});
+	// origina1 end
 
-  app.get("/login", (req, res) => {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
-  });
+	// try app.get '/'
+	app.get('/', function(req, res) {
+		burger.all(function(data) {
+			const hbsObject = {
+				burgers: data,
+			};
+			console.log(hbsObject);
+			res.render('index', hbsObject);
+		});
+	});
 
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
-  });
+	app.get('/', (req, res) => {
+		// If the user already has an account send them to the members page
+		if (req.user) {
+			// comment:
+			// if we use handlebars => we do not need to redirect them to members
+			// we can send the data directly to handlebars
+			res.redirect('/members');
+		}
+		res.sendFile(path.join(__dirname, '../public/signup.html'));
+	});
+	// try end
+
+	app.get('/login', (req, res) => {
+		// If the user already has an account send them to the members page
+		if (req.user) {
+			res.redirect('/members');
+		}
+		res.sendFile(path.join(__dirname, '../public/login.html'));
+	});
+
+	// Here we've add our isAuthenticated middleware to this route.
+	// If a user who is not logged in tries to access this route they will be redirected to the signup page
+	app.get('/members', isAuthenticated, (req, res) => {
+		res.sendFile(path.join(__dirname, '../public/members.html'));
+	});
 };
