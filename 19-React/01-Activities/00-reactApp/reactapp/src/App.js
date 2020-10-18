48c7f9a24dfbe323c22ae2373b8e4512fd8ab8fa
digilau <rickyfukfuk@gmail.com>
2020-10-18 01:57:58 -0700

    update

--- 19-React/01-Activities/00-reactApp/reactapp/src/App.js
+++ 19-React/01-Activities/00-reactApp/reactapp/src/App.js
@@ -1,41 +1,8 @@
 import React from "react";
-import List from "./components/List";
-
-const groceries = [
-  {
-    id: 1,
-    name: "Milk",
-    purchased: true
-  },
-  {
-    id: 2,
-    name: "Eggs",
-    purchased: true
-  },
-  {
-    id: 3,
-    name: "Cheese",
-    purchased: false
-  },
-  {
-    id: 4,
-    name: "Cake Mix",
-    purchased: false
-  },
-  {
-    id: 5,
-    name: "Carrots",
-    purchased: false
-  },
-  {
-    id: 6,
-    name: "Juice",
-    purchased: true
-  }
-];
+import OmdbContainer from "./components/OmdbContainer";
 
 function App() {
-  return <List groceries={groceries} />;
+  return <OmdbContainer />;
 }
 
 export default App;
